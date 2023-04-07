dbf4154522a41cdbd238106bb9685bc48a77c0d0
Parker <94702972+park-err@users.noreply.github.com>
2023-04-07 16:41:33 -0500

    committing practice with jquery

---
+++ jquery-effects/app.js
@@ -0,0 +1,12 @@
+$("#hide").click(function() {
+    $("#hide-show-par").hide()
+})
+$("#show").click(function() {
+    $("#hide-show-par").show()
+})
+$("#fade").click(function() {
+    $("#fade-par").fade()
+})
+$("#slide").click(function() {
+    $("#slide-par").slide()
+})
---
+++ jquery-start/app.js
@@ -0,0 +1,10 @@
+// hide paragraphs buttons
+$("#par-btn").click(function() {
+    $("p").hide();
+})
+$("#div-btn").click(function() {
+    $("#d").hide();
+})
+$(".clickme").click(function() {
+    $(this).hide();
+})
